305073da344d71b9b987a9adef33046645f2843e
vinaya K T <ktvinaya2001@gmail.com>
2025-08-11 20:45:25 +0530

    Add files via upload

--- db_connection.cs
+++ db_connection.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Data.SqlClient;
+using pothole_detection_system.DB_conn;
 
-namespace BLL_potholedete.DB_conn
+namespace pothole_detection_system.DB_conn
 {
     public class db_connection
     {
         public SqlConnection conn;
         public SqlCommand cmd;
+        public SqlDataAdapter da;
+        public SqlDataReader dr;
 
         public SqlConnection connect()
         {
             conn = new SqlConnection(@"Data Source=DESKTOP-7JFJ80D;Initial Catalog=pothole_detection;Integrated Security=True;");
             conn.Open();
-
             return conn;
         }
     }
